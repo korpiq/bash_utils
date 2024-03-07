10dbae24388637dfd10ab8dc4de2f4dc4f0b0d16
Kalle Hallivuori <kato@iki.fi>
2024-03-07 10:18:05 +0200

    Cosmetics in backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -14,17 +14,17 @@ usage () {
 readonly SOURCE_DIR="$1"
 readonly BACKUP_DIR="$2"
 
-readonly RUN="${DRY_RUN:+echo }$([ -w "$BACKUP_DIR" ] || echo sudo)"
-readonly DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
-readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
+readonly RUN="${DRY_RUN:+echo }$([ -w "${BACKUP_DIR}" ] || echo sudo)"
+readonly BACKUP_TIMESTAMP_DIRNAME="$(date '+%Y-%m-%d_%H:%M:%S')"
+readonly BACKUP_PATH="${BACKUP_DIR}/${BACKUP_TIMESTAMP_DIRNAME}"
 readonly LATEST_LINK="${BACKUP_DIR}/latest"
 
 ${RUN} rsync -va --delete \
-  "${SOURCE_DIR}" \
   --link-dest "${LATEST_LINK}" \
-  --filter "dir-merge .backup-filter.conf" \
+  --filter "dir-merge /.backup-filter.conf" \
+  "${SOURCE_DIR}" \
   "${BACKUP_PATH}"
 
-${RUN} rm -rf "${LATEST_LINK}"
-${RUN} ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
+${RUN} rm -f "${LATEST_LINK}"
+${RUN} ln -s "${BACKUP_TIMESTAMP_DIRNAME}" "${LATEST_LINK}"
 
